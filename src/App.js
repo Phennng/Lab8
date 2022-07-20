15a89ddaad4346a9098726fe60f3cf910a318204
Nguyễn Trọng Nhân <nhanntse150953@fpt.edu.vn>
2022-07-20 21:15:25 +0700

    Getting Started with React

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
 import { ConfigureStore } from "./redux/configureStore";
-import { Provider } from "react-redux";
 
 const store = ConfigureStore();
 class App extends Component {
