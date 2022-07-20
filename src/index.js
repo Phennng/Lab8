034031b83d19a5c35b4e4495b18cbd87bdd0ab76
Nguyễn Trọng Nhân <nhanntse150953@fpt.edu.vn>
2022-07-20 21:32:04 +0700

    Lab5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import ReactDOM from "react-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-social/bootstrap-social.css";
