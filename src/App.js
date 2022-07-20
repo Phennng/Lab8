afbf563f1edb8eb652af6fdaa64d2709a6ec38d7
Nguyễn Trọng Nhân <nhanntse150953@fpt.edu.vn>
2022-07-20 21:19:35 +0700

    Configuring your React Application

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
-import { ConfigureStore } from "./redux/configureStore";
 
 const store = ConfigureStore();
 class App extends Component {
