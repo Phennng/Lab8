f3095db934a965e4fb7833031fa262a51cb02f67
Nguyễn Trọng Nhân <nhanntse150953@fpt.edu.vn>
2022-07-20 21:25:42 +0700

    Uncontrolled Forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/MainComponent";
-import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 
